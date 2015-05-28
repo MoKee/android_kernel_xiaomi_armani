698d7b1f75ba8827b6abb7e6d0a9e6d1f67ab4fe
Riley Andrews <riandrews@android.com>
2015-05-28 12:10:05 -0700

    android: drivers: workaround debugfs race in binder
    If a /d/binder/proc/[pid] entry is kept open after linux has
    torn down the associated process, binder_proc_show can deference
    an invalid binder_proc that has been stashed in the debugfs
    inode.  Validate that the binder_proc ptr passed into binder_proc_show
    has not been freed by looking for it within the global process list
    whilst the global lock is held. If the ptr is not valid, print nothing.
    
    Bug 19587483
    Change-Id: Ice878c171db51ef9a4879c2f9299a2deb873d255
    Signed-off-by: Riley Andrews <riandrews@android.com>
    
    Conflicts:
    	drivers/staging/android/binder.c

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -3589,8 +3589,8 @@ static int binder_transactions_show(struct seq_file *m, void *unused)
 static int binder_proc_show(struct seq_file *m, void *unused)
 {
 	struct binder_proc *itr;
-	struct hlist_node *pos;
 	struct binder_proc *proc = m->private;
+	struct hlist_node *pos;
 	int do_lock = !binder_debug_no_lock;
 	bool valid_proc = false;
 
@@ -3607,6 +3607,7 @@ static int binder_proc_show(struct seq_file *m, void *unused)
 		seq_puts(m, "binder proc state:\n");
 		print_binder_proc(m, proc, 1);
 	}
+
 	if (do_lock)
 		mutex_unlock(&binder_lock);
 	return 0;
